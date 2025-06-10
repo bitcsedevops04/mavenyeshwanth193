553da5d708e7742e38985fdf677f917942d865ad
Maithri <bitcsedevops05@gmail.com>
2025-06-10 12:10:03 +0530

    initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Yeshwanth 1BI22CS193
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Yeshwanth 1BI22Cs193" );
+    }
+}
